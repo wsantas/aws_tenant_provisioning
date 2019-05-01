76c7718420a4473ddf286d6f6880889cca48cb54
William Santasiero <wsantasiero@accent-technologies.com>
2019-05-01 18:27:46 -0400

    ES-278 Tenant provisioning
    beef up tests. consider use case or services for BL

--- project/tenant_provisioning/tests/unit/tenant_model_tests.py
+++ project/tenant_provisioning/tests/unit/tenant_model_tests.py
@@ -10,7 +10,20 @@ class TenantModelTest(TestCase):
 
     def test_saving_and_retrieving_tenant(self):
         tenant = Tenant()
+        tenant.tenantId='ACME1234'
         tenant.save()
+
+        second_tenant = Tenant()
+        second_tenant.tenantId = 'XYZ444'
+        second_tenant.save()
+
+        saved_tenants = Tenant.objects.all()
+        self.assertEqual(saved_tenants.count(), 2)
+
+        first_saved_tenant = saved_tenants[0]
+        second_saved_tenant = saved_tenants[1]
+        self.assertEqual(first_saved_tenant.tenantId, 'ACME1234')
+        self.assertEqual(second_saved_tenant.tenantId, 'XYZ444')
 
 
 if __name__ == '__main__':
